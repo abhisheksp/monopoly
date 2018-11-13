23500d297a2b088b5050efc3684bcef8daa51529
Abhishek S P <abhisheksp1993@gmail.com>
2018-11-13 18:31:34 -0500

    #1: fix dice double roll definition

--- game_phases/dice_roll.py
+++ game_phases/dice_roll.py
@@ -1,14 +1,17 @@
 import game_phases.game_phase
-from random import seed, randint
 
 
 class DiceRoll(game_phases.game_phase.GamePhase):
-    def __init__(self):
-        seed(6690106822)
+    def __init__(self, dice):
+        self._dice = dice
+
+    @staticmethod
+    def _double_roll(roll):
+        return roll[0] == roll[1]
 
     def apply(self, game_state, action=None):
-        dice_roll = randint(1, 7)
-        if not game_state.current_player.double_roll(dice_roll):
+        dice_roll = self._dice.roll()
+        if not self._double_roll(dice_roll):
             game_state.next_player()
         game_state.current_player.update_roll(dice_roll)
         return self, game_state
