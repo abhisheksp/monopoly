f68a43330f09037f9aba169e78cf18b69896232a
Abhishek S P <abhisheksp1993@gmail.com>
2018-11-14 22:18:09 -0500

    #4: bsmt-diceroll transition

--- game_phases/bsmt.py
+++ game_phases/bsmt.py
@@ -6,4 +6,8 @@ class BSMT(game_phases.game_phase.GamePhase):
         pass
 
     def apply(self, game_context, action=None):
-        pass
+        game_state = game_context.state
+        if not game_state.current_player.double_roll():
+            game_state.next_player()
+        game_context.phase = game_context.get_phase('DiceRoll')
+        return game_context
