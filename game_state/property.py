f650d1fe754446ec1515db0dfb06ce87ab76ba69
Abhishek S P <abhisheksp1993@gmail.com>
2018-11-19 12:50:22 -0500

    #8: rent for houses/hotels

--- game_state/property.py
+++ game_state/property.py
@@ -2,26 +2,25 @@ from game_state.property_type import PropertyType
 
 
 class Property:
-    def __init__(self, name, color, cost, type_, rent=0, group=None, player=None):
+    def __init__(self, name, color, cost, type_, houses=0, rent=None, group=None, player=None):
         self.name = name
         self.color = color
         self.cost = cost
         self.owned_by = player
         self.type = type_
-        self.group = group
-        if group is None:
-            self.group = []
-        self._rent = rent
+        self.group = group or []
+        self._rent = rent or {}
+        self.houses = houses
 
     def own(self, player):
         self.owned_by = player
         self.type = PropertyType.OWNED
 
     def rent(self):
-        result = self._rent
+        result = self._rent[self.houses]
         group_owned = all(map(lambda property_: self.owned_by is property_.owned_by, self.group))
-        if self.group and group_owned:
-            result = self._rent * 2
+        if self.houses == 0 and self.group and group_owned:
+            result = self._rent[self.houses] * 2
         return result
 
     def __repr__(self):
--- game_state/tests/test_property.py
+++ game_state/tests/test_property.py
@@ -7,13 +7,27 @@ from game_state.property import Property
 
 class PropertyTest(TestCase):
     def test_regular_rent(self):
-        property_1 = Property('Mediterranean Ave', None, None, None, 200)
+        rent = {0: 200}
+        property_1 = Property('Mediterranean Ave', None, None, None, rent=rent)
         player = Player(1)
         property_1.own(player)
         self.assertEqual(property_1.rent(), 200)
 
     def test_double_rent(self):
-        property_1 = Property('Mediterranean Ave', None, None, None, 200)
+        rent = {0: 200}
+        property_1 = Property('Mediterranean Ave', None, None, None, rent=rent)
+        property_2 = Property('Baltic Ave', None, None, None, None)
+        player = Player(1)
+        property_1.own(player)
+        property_2.own(player)
+        property_1.group = [property_2]
+        property_2.group = [property_1]
+
+        self.assertEqual(property_1.rent(), 400)
+
+    def test_rent_with_houses(self):
+        rent = {1: 400}
+        property_1 = Property('Mediterranean Ave', None, None, None, houses=1, rent=rent)
         property_2 = Property('Baltic Ave', None, None, None, None)
         player = Player(1)
         property_1.own(player)
